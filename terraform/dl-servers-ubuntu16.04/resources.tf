39cc3c3a976f2256a83a8ccc5ad520e100f24de7
filipecosta90 <filipecosta.90@gmail.com>
2019-09-02 23:36:07 +0100

    [add] kicking of dl server infrastructure

---
+++ terraform/dl-servers-ubuntu16.04/resources.tf
@@ -0,0 +1,174 @@
+#providers
+provider "aws" {
+  region = "${var.region}"
+}
+
+data "terraform_remote_state" "shared_resources" {
+  backend = "s3"
+  config = {
+    bucket = "performance-cto-group"
+    key    = "benchmarks/infrastructure/shared_resources.tfstate"
+    region = "us-east-1"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "performance-cto-group"
+    key    = "benchmarks/infrastructure/perf-cto-DL-servers-ubuntu16.04.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
+resource "aws_network_interface" "perf_cto_server_c5n_9xlarge_network_interface" {
+  count           = "${var.instance_network_interface_plus_count}"
+  subnet_id       = data.terraform_remote_state.shared_resources.outputs.subnet_public_id
+  security_groups = ["${data.terraform_remote_state.shared_resources.outputs.performance_cto_sg_id}"]
+
+  attachment {
+    instance     = "${aws_instance.perf_cto_server_c5n_9xlarge[0].id}"
+    device_index = "${count.index + 2}"
+  }
+}
+
+resource "aws_instance" "perf_cto_server_c5n_9xlarge" {
+  count                  = "${var.server_instance_count}"
+  ami                    = "${var.instance_ami}"
+  instance_type          = "${var.instance_type}"
+  subnet_id              = data.terraform_remote_state.shared_resources.outputs.subnet_public_id
+  vpc_security_group_ids = ["${data.terraform_remote_state.shared_resources.outputs.performance_cto_sg_id}"]
+  key_name               = "${var.key_name}"
+  cpu_core_count         = "${var.instance_cpu_core_count}"
+
+  cpu_threads_per_core = "${var.instance_cpu_threads_per_core_hyperthreading}"
+  placement_group      = "${data.terraform_remote_state.shared_resources.outputs.perf_cto_pg_name}"
+
+  ebs_block_device {
+    device_name           = "${var.instance_device_name}"
+    volume_size           = "${var.instance_volume_size}"
+    volume_type           = "io1"
+    iops                  = "${var.instance_volume_iops}"
+    encrypted             = false
+    delete_on_termination = true
+    
+  }
+
+  volume_tags = {
+    Name = "ebs_block_device-${var.setup_name}-${count.index + 1}"
+    RedisModule = "${var.redis_module}"
+  }
+
+  tags = {
+    Name = "${var.setup_name}-${count.index + 1}"
+    RedisModule = "${var.redis_module}"
+  }
+
+  # Ansible requires Python to be installed on the remote machine as well as the local machine.
+  provisioner "remote-exec" {
+    inline = ["sudo apt install python -y"]
+    connection {
+      host        = "${self.public_ip}" # The `self` variable is like `this` in many programming languages
+      type        = "ssh"               # in this case, `self` is the resource (the server).
+      user        = "${var.ssh_user}"
+      private_key = "${file(var.private_key)}"
+    }
+  }
+
+  #################
+  # EC2 CONFIGURE #
+  #################
+  # provisioner "local-exec" {
+  #   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/ec2-configure.yml -i ${self.public_ip},"
+  # }
+
+  # ################################################################################
+  # # performance related
+  # ################################################################################
+  ########
+  # PERF #
+  ########
+  # provisioner "local-exec" {
+  #   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/perf.yml -i ${self.public_ip},"
+  # }
+
+  # #######
+  # # BCC #
+  # #######
+  # provisioner "local-exec" {
+  #   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/bcc.yml -i ${self.public_ip},"
+  # }
+
+
+  # #######
+  # # PCP #
+  # #######
+  # provisioner "local-exec" {
+  #   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/pcp.yml -i ${self.public_ip},"
+  # }
+  
+
+  # ###########
+  # # NETPERF #
+  # ###########
+  # provisioner "local-exec" {
+  #   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key ${var.private_key} ../../playbooks/${var.os}/netperf.yml -i ${self.public_ip},"
+  # }
+
+
+  # ####################################
+  # # TUNED-ADM THROUGHPUT PERFORMANCE #
+  # ####################################
+  # provisioner "local-exec" {
+  #   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/tuned.yml -i ${self.public_ip},"
+  # }
+
+
+  ###################
+  # SYSCTL SETTINGS #
+  ###################
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/sysctl.yml -i ${self.public_ip},"
+  }
+
+
+  ####################################
+  # DISABLING TRANSPARENT HUGE PAGES #
+  ####################################
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/thp.yml -i ${self.public_ip},"
+  }
+
+  # ###############################################
+  # # PCP VECTOR PANDA - System Wide Flame Graphs #
+  # ###############################################
+  # provisioner "local-exec" {
+  #   command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ec2-user --private-key ${var.private_key} ../../playbooks/${var.os}/pcp-vector-pmda.yml -i ${self.public_ip},"
+  # }
+
+  #########################
+  # Install node exporter #
+  #########################
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/node-exporter.yml -i ${self.public_ip},"
+  }
+
+   ################################################################################
+  # Redis AI OSS related
+  ################################################################################
+
+  ###############
+  # Install RedisAI OSS #
+  ###############
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/redis-ai-oss.yml -i ${self.public_ip}, --extra-vars \"redis_ai_version=${var.redis_ai_oss_version}\""
+  }
+
+  ###############
+  # Install Redis OSS #
+  ###############
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/redis-server-oss-ai.yml -i ${self.public_ip}, --extra-vars \"redis_version=${var.redis_oss_version} redis_ai_module_path=~/redisai_src/build/redisai.so\""
+  }
+
+}
---
+++ terraform/dl-servers-ubuntu16.04/variables.tf
@@ -0,0 +1,115 @@
+# Variables
+
+variable "setup_name" {
+  description = "setup name"
+  default = "perf-cto-dl-servers-ubuntu16.04"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "server_instance_count" {
+  default = "1"
+}
+
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance - Deep Learning AMI (Ubuntu)"
+  default     = "ami-0944c173745e93dff"
+}
+
+variable "instance_volume_size" {
+    description = "EC2 instance volume_size"
+  default = "128"
+}
+
+variable "instance_volume_iops" {
+    description = "EC2 instance volume_iops"
+  default = "100"
+}
+
+variable "instance_device_name" {
+    description = "EC2 instance device name"
+  default = "/dev/sda1"
+}
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default     = "c5n.9xlarge"
+}
+
+variable "instance_type_5m" {
+  description = "type for aws EC2 instance"
+  default     = "c5.9xlarge"
+}
+
+variable "instance_cpu_core_count" {
+  description = "CPU core count for aws EC2 instance"
+  default     = 18
+}
+
+variable "instance_cpu_threads_per_core" {
+  description = "CPU threads per core for aws EC2 instance"
+  default     = 1
+}
+
+
+variable "instance_cpu_threads_per_core_hyperthreading" {
+  description = "CPU threads per core when hyperthreading is enabled for aws EC2 instance"
+  default     = 2
+}
+
+
+
+variable "instance_network_interface_plus_count" {
+  description = "number of additional network interfaces to add to aws EC2 instance"
+  default     = 0
+}
+
+
+variable "os" {
+ description = "os"
+  default     = "ubuntu16.04"
+}
+
+
+variable "ssh_user" {
+ description = "ssh_user"
+  default     = "ubuntu"
+}
+
+
+variable "private_key" {
+ description = "private key"
+  default     = "./../../../pems/performance-cto-us-east-2.pem"
+}
+
+
+variable "public_key" {
+   description = "public key"
+  default     = "./../../../pems/performance-cto-us-east-2.pub"
+}
+
+
+variable "key_name" {
+   description = "key name"
+  default     = "performance-cto-us-east-2"
+}
+
+
+variable "redis_oss_version" {
+ description = "redis_oss_version"
+  default     = "5.0.4"
+}
+
+variable "redis_ai_oss_version" {
+ description = "redis_ai_oss_version"
+  default     = "v0.3.1"
+}
+
+variable "redis_module" {
+ description = "redis_module"
+  default     = "RedisAI"
+}
+
+
+
