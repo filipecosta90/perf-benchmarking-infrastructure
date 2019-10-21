96e5d09b268f4f990da3b56d977e0559fea6b1a7
filipecosta90 <filipecosta.90@gmail.com>
2019-10-21 10:55:25 +0100

    [add] adding rhel7 redisearch 10 shard setup

---
+++ terraform/RE-servers-amazonlinux2-redisearch/resources.tf
@@ -0,0 +1,169 @@
+#providers
+provider "aws" {
+  region = "${var.region}"
+}
+
+data "terraform_remote_state" "shared_resources" {
+  backend = "s3"
+  config = {
+    bucket = "performance-cto-group"
+    key    = "benchmarks/infrastructure/shared_resources.tfstate"
+    region = "us-east-1"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "performance-cto-group"
+    key    = "benchmarks/infrastructure/perf-cto-RE-servers-amazonlinux2-redisearch.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_network_interface" "perf_cto_server_c5n_9xlarge_network_interface" {
+  count           = "${var.instance_network_interface_plus_count}"
+
+  subnet_id              = data.terraform_remote_state.shared_resources.outputs.subnet_public_id
+  security_groups = ["${data.terraform_remote_state.shared_resources.outputs.performance_cto_sg_id}"]
+
+  attachment {
+    instance     = "${aws_instance.perf_cto_server_c5n_9xlarge[0].id}"
+    device_index = "${count.index + 2}"
+  }
+
+  tags = {
+    Name = "eni-${var.setup_name}-${count.index + 2}"
+    RedisModule = "${var.redis_module}"
+  } 
+
+}
+
+resource "aws_instance" "perf_cto_server_c5n_9xlarge" {
+  count                  = "${var.server_instance_count}"
+  ami                    = "${var.instance_ami}"
+  instance_type          = "${var.instance_type}"
+  subnet_id              = data.terraform_remote_state.shared_resources.outputs.subnet_public_id
+  vpc_security_group_ids = ["${data.terraform_remote_state.shared_resources.outputs.performance_cto_sg_id}"]
+  key_name               = "${var.key_name}"
+  cpu_core_count         = "${var.instance_cpu_core_count}"
+  cpu_threads_per_core = "${var.instance_cpu_threads_per_core_hyperthreading}"
+  placement_group      = "${data.terraform_remote_state.shared_resources.outputs.perf_cto_pg_name}"
+  private_ip = "10.3.0.${count.index + 60}"
+
+root_block_device {
+    volume_size           = "${var.instance_volume_size}"
+    volume_type           = "${var.instance_volume_type}"
+    iops                  = "${var.instance_volume_iops}"
+    encrypted             = "${var.instance_volume_encrypted}"
+    delete_on_termination = true
+  }
+
+  volume_tags = {
+    Name        = "ebs_block_device-${var.setup_name}-${count.index + 1}"
+    RedisModule = "${var.redis_module}"
+  }
+
+  tags = {
+    Name        = "${var.setup_name}-${count.index + 1}"
+    RedisModule = "${var.redis_module}"
+  }
+
+  # wait for instance to be ready to receive connection
+  provisioner "remote-exec" {
+    script = "./../../scripts/wait_for_instance.sh"
+    connection {
+      host        = "${self.public_ip}" # The `self` variable is like `this` in many programming languages
+      type        = "ssh"               # in this case, `self` is the resource (the server).
+      user        = "${var.ssh_user}"
+      private_key = "${file(var.private_key)}"
+      #need to increase timeout to larger then 5m for metal instances
+      timeout = "15m"
+    }
+
+  }
+
+  ################################################################################
+  # performance related
+  ################################################################################
+  
+  #################
+  # EC2 CONFIGURE #
+  #################
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/ec2-configure.yml -i ${self.public_ip},"
+  }
+
+  ###########
+  # NETPERF #
+  ###########
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/netperf.yml -i ${self.public_ip},"
+  }
+
+  ####################################
+  # TUNED-ADM THROUGHPUT PERFORMANCE #
+  ####################################
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/tuned.yml -i ${self.public_ip},"
+  }
+
+  ###################
+  # SYSCTL SETTINGS #
+  ###################
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/sysctl.yml -i ${self.public_ip},"
+  }
+
+
+  ####################################
+  # DISABLING TRANSPARENT HUGE PAGES #
+  ####################################
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/thp.yml -i ${self.public_ip},"
+  }
+
+  #########################
+  # Install node exporter #
+  #########################
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/node-exporter.yml -i ${self.public_ip},"
+  }
+
+  ###################
+  # Install netdata #
+  ###################
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/netdata.yml -i ${self.public_ip},"
+  }
+
+  ################################################################################
+  # Redis Enterprise related
+  ################################################################################
+
+  ###############################################
+  # Create inventory file #
+  ###############################################
+  provisioner "local-exec" {
+
+    command = "echo $l1 >> ${self.public_ip}.inv && echo $l2 >> ${self.public_ip}.inv"
+
+    environment = {
+      l1 = "[re_master]"
+      l2 = "${self.public_ip}"
+    }
+  }
+
+  ##############
+  # Install RE #
+  ##############
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u ${var.ssh_user} --private-key ${var.private_key} ../../playbooks/${var.os}/1VM-redis-enterprise.yml -i ${self.public_ip}.inv"
+  }
+
+  ###############################################
+  # Remove inventory file #
+  ###############################################
+  provisioner "local-exec" {
+    command = "rm ${self.public_ip}.inv"
+  }
+}
---
+++ terraform/RE-servers-amazonlinux2-redisearch/variables.tf
@@ -0,0 +1,136 @@
+# Variables
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "setup_name" {
+  description = "setup name"
+  default = "perf-cto-RE-servers-amazonlinux2-redisearch"
+}
+
+
+variable "redis_module" {
+ description = "redis_module"
+  default     = "RediSearch"
+}
+
+variable "os" {
+ description = "os"
+  default     = "rhel7"
+}
+
+variable "ssh_user" {
+ description = "ssh_user"
+  default     = "ec2-user"
+}
+
+variable "server_instance_count" {
+  default = "1"
+}
+variable "client_instance_count" {
+  default = "1"
+}
+
+variable "cidr_vpc" {
+  description = "CIDR block for the VPC"
+  default     = "10.3.0.0/16"
+}
+variable "cidr_subnet" {
+  description = "CIDR block for the subnet"
+  default     = "10.3.0.0/24"
+}
+variable "availability_zone" {
+  description = "availability zone to create subnet"
+  default     = "us-east-2a"
+}
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance"
+  default     = "ami-0d8f6eb4f641ef691"
+}
+
+variable "instance_device_name" {
+  description = "EC2 instance device name"
+  default     = "/dev/xvda"
+}
+
+variable "instance_volume_size" {
+    description = "EC2 instance volume_size"
+  default = "128"
+}
+
+variable "instance_volume_encrypted" {
+  description = "EC2 instance instance_volume_encrypted"
+  default     = "false"
+}
+
+variable "instance_volume_iops" {
+    description = "EC2 instance volume_iops"
+  default = "100"
+}
+
+variable "instance_volume_type" {
+    description = "EC2 instance volume_type"
+  default = "gp2"
+}
+
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default     = "c5.18xlarge"
+}
+
+variable "instance_type_5m" {
+  description = "type for aws EC2 instance"
+  default     = "c5.18xlarge"
+}
+
+variable "instance_cpu_core_count" {
+  description = "CPU core count for aws EC2 instance"
+  default     = 36
+}
+
+variable "instance_cpu_threads_per_core" {
+  description = "CPU threads per core for aws EC2 instance"
+  default     = 2
+}
+
+
+variable "instance_cpu_threads_per_core_hyperthreading" {
+  description = "CPU threads per core when hyperthreading is enabled for aws EC2 instance"
+  default     = 2
+}
+
+
+variable "instance_network_interface_plus_count" {
+  description = "number of additional network interfaces to add to aws EC2 instance"
+  default     = 0
+}
+
+
+variable "placement_group_name" {
+  description = "placement group name"
+  default     = "perf-cto-pg"
+}
+
+variable "environment_tag" {
+  description = "Environment tag"
+  default     = "performance-cto"
+}
+
+variable "private_key" {
+ description = "private key"
+  default     = "./../../../pems/performance-cto-us-east-2.pem"
+}
+
+
+variable "public_key" {
+   description = "public key"
+  default     = "./../../../pems/performance-cto-us-east-2.pub"
+}
+
+variable "key_name" {
+   description = "key name"
+  default     = "performance-cto-us-east-2"
+}
+
+
