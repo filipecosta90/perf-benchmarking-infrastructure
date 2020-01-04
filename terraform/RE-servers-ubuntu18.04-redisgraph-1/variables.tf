13972943729b5971b0caa5c3a4cc292e943a4d62
filipecosta90 <filipecosta.90@gmail.com>
2020-01-04 12:18:34 +0000

    [add] working on base debug image on ubuntu 18.04

---
+++ terraform/RE-servers-ubuntu18.04-redisgraph-1/variables.tf
@@ -0,0 +1,127 @@
+# Variables
+
+variable "setup_name" {
+  description = "setup name"
+  default     = "perf-cto-RE-servers-ubuntu18.04-redisgraph"
+}
+
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "server_instance_count" {
+  default = "1"
+}
+
+variable "instance_ami" {
+  description = "AMI for aws EC2 instance - us-east-2 Ubuntu 18.04 LTS ami-021b7b04f1ac696c2"
+  default     = "ami-021b7b04f1ac696c2"
+}
+
+variable "instance_device_name" {
+  description = "EC2 instance device name"
+  default     = "/dev/sda1"
+}
+
+variable "redis_module" {
+  description = "redis_module"
+  default     = "RedisGraph"
+}
+
+
+variable "re_cluster_name" {
+ description = "re_cluster_name"
+  default     = "cluster-1vm-rhel7-redisgraph-1"
+}
+
+variable "instance_volume_size" {
+  description = "EC2 instance volume_size"
+  default     = "128"
+}
+
+
+variable "instance_volume_type" {
+  description = "EC2 instance volume_type"
+  default     = "gp2"
+}
+
+variable "instance_volume_iops" {
+  description = "EC2 instance volume_iops"
+  default     = "384"
+}
+
+variable "instance_volume_encrypted" {
+  description = "EC2 instance instance_volume_encrypted"
+  default     = "false"
+}
+
+variable "instance_root_block_device_encrypted" {
+  description = "EC2 instance instance_root_block_device_encrypted"
+  default     = "false"
+}
+
+# Model	c5.24xlarge	
+# Processor	Xeon Platinum 8000
+# (Skylake-SP)	Xeon Platinum 8000
+# vCPU	96
+# Memory (GiB) 196
+# Instance Storage (GiB)	EBS-Only	
+# Network Bandwidth (Gbps) 100
+variable "instance_type" {
+  description = "type for aws EC2 instance"
+  default     = "c5.24xlarge"
+}
+
+variable "instance_cpu_core_count" {
+  description = "CPU core count for aws EC2 instance"
+  default     = 48
+}
+
+variable "instance_cpu_threads_per_core" {
+  description = "CPU threads per core for aws EC2 instance"
+  default     = 1
+}
+
+
+variable "instance_cpu_threads_per_core_hyperthreading" {
+  description = "CPU threads per core when hyperthreading is enabled for aws EC2 instance"
+  default     = 2
+}
+
+
+variable "instance_network_interface_plus_count" {
+  description = "number of additional network interfaces to add to aws EC2 instance"
+  default     = 0
+}
+
+
+variable "os" {
+  description = "os"
+  default     = "ubuntu16.04"
+}
+
+
+variable "ssh_user" {
+  description = "ssh_user"
+  default     = "ubuntu"
+}
+
+
+variable "private_key" {
+  description = "private key"
+  default     = "./../../../pems/performance-cto-us-east-2.pem"
+}
+
+
+variable "public_key" {
+  description = "public key"
+  default     = "./../../../pems/performance-cto-us-east-2.pub"
+}
+
+
+variable "key_name" {
+  description = "key name"
+  default     = "performance-cto-us-east-2"
+}
+
+
