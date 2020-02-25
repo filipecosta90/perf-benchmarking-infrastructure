c201da7338b3c1ccf7444dbb0b289eae097e83c3
filipecosta90 <filipecosta.90@gmail.com>
2020-02-25 15:04:51 +0000

    [add] adding confing for OSS neo4j 3.3.6 deployment

---
+++ terraform/OSS-servers-ubuntu18.04-neo4j-3.3.6/resources.tf
@@ -0,0 +1,67 @@
+#providers
+provider "aws" {
+  region = "${var.region}"
+}
+
+data "terraform_remote_state" "shared_resources" {
+  backend = "s3"
+  config = {
+    bucket = "performance-cto-group"
+    key    = "benchmarks/infrastructure/shared_resources.tfstate"
+    region = "us-east-1"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "performance-cto-group"
+    key    = "benchmarks/infrastructure/perf-cto-OSS-servers-ubuntu18.04-neo4j-3.3.6.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "perf_cto_server" {
+  count                  = "${var.server_instance_count}"
+  ami                    = "${var.instance_ami}"
+  instance_type          = "${var.instance_type}"
+  subnet_id              = data.terraform_remote_state.shared_resources.outputs.subnet_public_id
+  vpc_security_group_ids = ["${data.terraform_remote_state.shared_resources.outputs.performance_cto_sg_id}"]
+  key_name               = "${var.key_name}"
+  cpu_core_count         = "${var.instance_cpu_core_count}"
+
+  cpu_threads_per_core = "${var.instance_cpu_threads_per_core}"
+  placement_group      = "${data.terraform_remote_state.shared_resources.outputs.perf_cto_pg_name}"
+
+  root_block_device {
+    volume_size           = "${var.instance_volume_size}"
+    volume_type           = "${var.instance_volume_type}"
+    iops                  = "${var.instance_volume_iops}"
+    encrypted             = "${var.instance_volume_encrypted}"
+    delete_on_termination = true
+  }
+
+  volume_tags = {
+    Name        = "ebs_block_device-${var.setup_name}-${count.index + 1}"
+    RedisModule = "${var.redis_module}"
+  }
+
+  tags = {
+    Name        = "${var.setup_name}-${count.index + 1}"
+    RedisModule = "${var.redis_module}"
+  }
+
+  # wait for instance to be ready to receive connection
+  provisioner "remote-exec" {
+    script = "./../../scripts/wait_for_instance.sh"
+    connection {
+      host        = "${self.public_ip}" # The `self` variable is like `this` in many programming languages
+      type        = "ssh"               # in this case, `self` is the resource (the server).
+      user        = "${var.ssh_user}"
+      private_key = "${file(var.private_key)}"
+      #need to increase timeout to larger then 5m for metal instances
+      timeout = "15m"
+    }
+
+  }
+
+}
